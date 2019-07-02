0b891cf04e49b4612f6300402a40ff990d329ad1
Dimitris Karakasilis <dkarakasilis@suse.com>
2019-07-02 11:33:03 +0200

    Set the hostpath of the cluster in the build dir
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>

--- scripts/up.sh
+++ scripts/up.sh
@@ -2,5 +2,19 @@
 set -x
 
 pushd build
-    ./kind create cluster
+    APPLICATION_PATH=$PWD
+
+    cat > kind-config.yaml <<EOF
+kind: Config
+apiVersion: kind.sigs.k8s.io/v1alpha2
+nodes:
+- role: control-plane
+  replicas: 1
+  extraMounts:
+    - containerPath: /code
+      hostPath: ${APPLICATION_PATH}
+      # readOnly: true
+EOF
+    ./kind create cluster --config kind-config.yaml
+
 popd
