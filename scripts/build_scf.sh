168a8baeccb26702934d1002ab9e3a81264b87af
Ettore Di Giacinto <edigiacinto@suse.com>
2019-07-03 12:35:32 +0200

    Add also git

--- scripts/build_scf.sh
+++ scripts/build_scf.sh
@@ -12,7 +12,7 @@ pushd scf
     git checkout "$SCF_BRANCH"
     git submodule sync --recursive && git submodule update --init --recursive && git submodule foreach --recursive "git checkout . && git reset --hard && git clean -dffx"
     #git submodule update --recursive --force && git submodule foreach --recursive 'git checkout . && git clean -fdx'
-    docker exec -ti $cluster_name-control-plane /bin/bash -c 'apt-get update && apt-get install wget make; cd /code/scf && source .envrc && ./bin/dev/install_tools.sh && make vagrant-prep'
+    docker exec -ti $cluster_name-control-plane /bin/bash -c 'apt-get update && apt-get install -y git wget make; cd /code/scf && source .envrc && ./bin/dev/install_tools.sh && make vagrant-prep'
     cp -rfv output/* ../
 popd
 
