2e3bfca7e946a17972c9e0e40608983754e19bdd
Ettore Di Giacinto <mudler@gentoo.org>
2019-07-27 15:34:52 +0200

    Get latest public chart if none specified

--- scripts/chart.sh
+++ scripts/chart.sh
@@ -3,6 +3,11 @@ set -ex
 
 . scripts/include/common.sh
 
+if [ -z "$CHART_URL" ]; then
+    echo "No chart url given - using latest public release from GH" 
+    CHART_URL=$(curl -s https://api.github.com/repos/SUSE/scf/releases/latest | grep "browser_download_url.*zip" | cut -d : -f 2,3 | tr -d \" | tr -d " ")
+fi
+
 wget "$CHART_URL" -O chart.zip
 
-unzip chart.zip
+unzip -o chart.zip
