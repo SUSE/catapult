4df9f7ca83281303710cc4acc67093e5447f5fb0
Ettore Di Giacinto <edigiacinto@suse.com>
2019-08-14 18:16:04 +0200

    Move eirinifs to the mounted path

--- scripts/eirinifs.sh
+++ scripts/eirinifs.sh
@@ -23,8 +23,9 @@ popd
 cp -rfv diego-ssh/cmd/sshd/sshd eirinifs/image
 pushd eirinifs
 
-docker run --rm --privileged -it --workdir / -v $PWD:/eirinifs eirini/ci /bin/bash -c "/eirinifs/ci/build-eirinifs/task.sh" 
-sudo chmod 777 image/eirinifs.tar &&  kubectl cp image/eirinifs.tar scf/bits-0:/var/vcap/store/bits-service/assets/eirinifs.tar 
+docker run --rm --privileged -it --workdir / -v $PWD:/eirinifs eirini/ci /bin/bash -c "/eirinifs/ci/build-eirinifs/task.sh && mv /go/src/github.com/cloudfoundry-incubator/eirinifs/image/eirinifs.tar /eirinifs/image"
 
 popd
+sudo chmod 777 image/eirinifs.tar &&  kubectl cp image/eirinifs.tar scf/bits-0:/var/vcap/store/bits-service/assets/eirinifs.tar 
+
 kubectl exec -it -n scf bits-0 -- bash -c -l "monit restart bits-service"
