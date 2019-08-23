0722aee98abd7daf91538ee4ad7fdfe26f94c1a1
Ettore Di Giacinto <edigiacinto@suse.com>
2019-08-23 16:03:44 +0200

    Use the zipped bundled chart
    Signed-off-by: Dimitris Karakasilis <dkarakasilis@suse.com>

--- scripts/build_scf.sh
+++ scripts/build_scf.sh
@@ -11,12 +11,13 @@ pushd scf
     git submodule sync --recursive && \
     git submodule update --init --recursive && \
     git submodule foreach --recursive "git checkout . && git reset --hard && git clean -dffx"
-
+    sed -i 's|exit 1||' make/kube-dist || true
+    sed -i 's|exit 1||' make/bundle-dist || true
     chmod 777 -R src/uaa-fissile-release/ || true
     docker exec ${DOCKER_OPTS} \
     -ti $cluster_name-control-plane \
-    /bin/bash -c 'apt-get update && apt-get install -y ruby git wget make; cd /code/scf && source .envrc && ./bin/dev/install_tools.sh && make vagrant-prep'
-    cp -rfv output/helm ../
-    cp -rfv output/kube ../
+    /bin/bash -c 'apt-get update && apt-get install -y ruby git wget make; cd /code/scf && source .envrc && ./bin/dev/install_tools.sh && make vagrant-prep bundle-dist'
+    cp -rfv output/*.zip ../
 popd
 
+unzip -o *.zip
