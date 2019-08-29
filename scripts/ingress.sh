5ee5aa6d1700968e3d29af73e3e5102d6c1e2bba
Ettore Di Giacinto <edigiacinto@suse.com>
2019-08-29 20:50:40 +0200

    Wait for the ingress to be up

--- scripts/ingress.sh
+++ scripts/ingress.sh
@@ -5,5 +5,10 @@ set -ex
 . scripts/include/common.sh
 
 kubectl apply -f ../kube/socks.yaml
+
+sleep 5
+
+bash ../scripts/wait.sh default
+
 echo "Now you can run: make ingress-forward &"
 echo "Afterwards you can access your cluster network by setting socks5://127.0.0.1:2224 as your proxy. e.g. https_proxy=socks5://127.0.0.1:2224"
