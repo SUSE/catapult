a5ae5b42963dc084856ecfa31374d63db2ba0d4b
Ettore Di Giacinto <edigiacinto@suse.com>
2019-08-29 17:56:55 +0200

    Add login via ingress proxy

--- scripts/login.sh
+++ scripts/login.sh
@@ -3,6 +3,10 @@ set -ex
 
 . scripts/include/common.sh
 
+if [ -n "$EKCP_PROXY" ]; then
+  export https_proxy=socks5://127.0.0.1:2224
+fi
+
 cf login --skip-ssl-validation -a https://api.${DOMAIN} -u admin -p ${CLUSTER_PASSWORD} -o system
 cf create-space tmp
 cf target -s tmp
