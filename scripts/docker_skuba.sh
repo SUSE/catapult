ccefcf9cadc76dce9d38edef43edb20f01ccb31d
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-09-02 10:25:21 +0200

    Fix make call in scripts/docker/skuba.sh

--- scripts/docker_skuba.sh
+++ scripts/docker_skuba.sh
@@ -5,5 +5,5 @@ set -xeuo pipefail
 . scripts/include/caasp4os.sh
 
 if [[ "$(docker images -q skuba/$CAASP_VER 2> /dev/null)" == "" ]]; then
-		make -C "$CF_CI_DIR"/docker/skuba/ "$CAASP_VER"
+		make -C caasp4/docker/skuba/ "$CAASP_VER"
 fi
