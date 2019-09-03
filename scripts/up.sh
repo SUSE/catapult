06b7eeb07b988d4a7266a1af4496119d603d6246
Dimitris Karakasilis <DKarakasilis@suse.com>
2019-09-03 17:10:40 +0300

    Don't pushd. common.sh script already does that.

--- scripts/up.sh
+++ scripts/up.sh
@@ -2,27 +2,25 @@
 set -x
 . scripts/include/common.sh
 
-pushd build${CLUSTER_NAME}
-    APPLICATION_PATH=$PWD
-    type="Config"
-    if [ "${KIND_APIVERSION}" == "kind.sigs.k8s.io/v1alpha3" ]; then
-        type="Cluster"
-    fi
-    cat > kind-config.yaml <<EOF
+APPLICATION_PATH=$PWD
+type="Config"
+if [ "${KIND_APIVERSION}" == "kind.sigs.k8s.io/v1alpha3" ]; then
+    type="Cluster"
+fi
+cat > kind-config.yaml <<EOF
 kind: $type
 apiVersion: ${KIND_APIVERSION}
 nodes:
 - role: control-plane
-  replicas: 1
-  extraMounts:
-    - containerPath: /code
-      hostPath: ${APPLICATION_PATH}
-      # readOnly: true
+replicas: 1
+extraMounts:
+- containerPath: /code
+  hostPath: ${APPLICATION_PATH}
+  # readOnly: true
 EOF
 
-    if [ -n "$EKCP_HOST" ]; then
-        curl -d "name=${CLUSTER_NAME}" -X POST http://$EKCP_HOST/new
-    else
-        ./kind create cluster --config kind-config.yaml --name=${CLUSTER_NAME}
-    fi
-popd
+if [ -n "$EKCP_HOST" ]; then
+    curl -d "name=${CLUSTER_NAME}" -X POST http://$EKCP_HOST/new
+else
+    ./kind create cluster --config kind-config.yaml --name=${CLUSTER_NAME}
+fi
