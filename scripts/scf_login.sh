703fd10be2e7ecc234ff715b0b1e2872619e9636
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-09-05 12:51:08 +0200

    Create .cf on scripts/scf_login.sh, cfcli doesn't create it

--- scripts/scf_login.sh
+++ scripts/scf_login.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-set -ex 
+set -ex
 
 . scripts/include/common.sh
 . .envrc
 
 if [ -n "$EKCP_PROXY" ]; then
-  export https_proxy=socks5://127.0.0.1:2224
+    export https_proxy=socks5://127.0.0.1:2224
 fi
 
 domain=$(kubectl get configmap -n kube-system cap-values -o json | jq -r '.data["domain"]')
 
+mkdir -p "$CF_HOME"
 cf login --skip-ssl-validation -a https://api."$domain" -u admin -p ${CLUSTER_PASSWORD} -o system
 cf create-space tmp
 cf target -s tmp
