389e1f9ce56a29dd62f842cb1f8a8018d25fd2c6
Ettore Di Giacinto <edigiacinto@suse.com>
2019-09-17 13:47:20 +0200

    Automatically set latest 'stable' chart for SCFv3
    And add support for local url and tgzs with CHART_URL #3

--- scripts/scf_chart.sh
+++ scripts/scf_chart.sh
@@ -5,9 +5,26 @@ set -ex
 
 if [ -z "$CHART_URL" ]; then
     echo "No chart url given - using latest public release from GH" 
-    CHART_URL=$(curl -s https://api.github.com/repos/SUSE/scf/releases/latest | grep "browser_download_url.*zip" | cut -d : -f 2,3 | tr -d \" | tr -d " ")
+    if  [ "${SCF_OPERATOR}" != "true" ]; then
+        CHART_URL=$(curl -s https://api.github.com/repos/SUSE/scf/releases/latest | grep "browser_download_url.*zip" | cut -d : -f 2,3 | tr -d \" | tr -d " ")
+    else
+        CHART_URL="https://scf-v3.s3.amazonaws.com/scf-3.0.0-8f7a71d1.tgz"
+        #CHART_URL="https://github.com/SUSE/scf/archive/v3-develop.zip"
+    fi
 fi
 
-wget "$CHART_URL" -O chart.zip
+if echo "$CHART_URL" | grep -q "http"; then
+    wget "$CHART_URL" -O chart
+else
+    cp -rfv "$CHART_URL" chart
+fi
 
-unzip -o chart.zip
+if echo "$CHART_URL" | grep -q "tgz"; then
+    tar -xvf chart -C ./
+else
+    unzip -o chart
+fi
+
+if  [ "${SCF_OPERATOR}" == "true" ]; then
+    cp -rfv scf*/* ./
+fi
