10ea22631923003742bf129856892133286dfbce
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-09-20 14:15:52 +0200

    Configure minikube requirements

--- scripts/minikube_up.sh
+++ scripts/minikube_up.sh
@@ -12,6 +12,11 @@ else
     MINIKUBE_VM_DRIVER=kvm2
 fi
 
+minikube config set WantUpdateNotification false
+minikube config set memory 8192
+minikube config set cpus $(($(nproc) - 2))
+minikube config set disk-size 60g
+
 minikube start \
          --container-runtime="$MINIKUBE_RUNTIME" \
          --vm-driver="$MINIKUBE_VM_DRIVER" \
