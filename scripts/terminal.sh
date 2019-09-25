dfa8986d2fb660db4708614232bdd3d6fe2cf7ea
Ettore Di Giacinto <edigiacinto@suse.com>
2019-09-25 11:18:51 +0200

    Minor fixup to terminal target

--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -3,7 +3,7 @@
 . scripts/include/common.sh
 . .envrc
 
-set -exuo pipefail
+set -euo pipefail
 
 kubectl create namespace bkindwscf || true
 kubectl delete -f ../kube/task.yaml || true
@@ -12,7 +12,11 @@ kubectl create -f ../kube/task.yaml
 bash ../scripts/wait_ns.sh bkindwscf
 
 kubectl cp ../build$CLUSTER_NAME bkindwscf/task:/bkindwscf/
-kubectl cp $TASK_SCRIPT bkindwscf/task:/bkindwscf/build$CLUSTER_NAME/
-kubectl exec -ti -n bkindwscf task -- bash -c "CLUSTER_NAME=$CLUSTER_NAME make buildir login"
+kubectl exec -ti -n bkindwscf task -- /bin/bash -c "CLUSTER_NAME=$CLUSTER_NAME make buildir login"
 
-exec kubectl exec -ti task -n default -- /bin/bash
+echo
+echo "@@@@@@@@@@@@@@"
+echo "Run: source build$CLUSTER_NAME/.envrc"
+echo "@@@@@@@@@@@@@@"
+echo
+exec kubectl exec -ti task -n bkindwscf -- /bin/bash
