69e1e4930f5ae0b2bbdb0578f699eafb714f62da
Ettore Di Giacinto <edigiacinto@suse.com>
2019-09-25 14:55:08 +0200

    Make the terminal pod persistent

--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -6,17 +6,23 @@
 set -euo pipefail
 
 kubectl create namespace bkindwscf || true
-kubectl delete -f ../kube/task.yaml || true
-kubectl create -f ../kube/task.yaml 
+kubectl create -f ../kube/task.yaml || true
 
 bash ../scripts/wait_ns.sh bkindwscf
 
 kubectl cp ../build$CLUSTER_NAME bkindwscf/task:/bkindwscf/
 kubectl exec -ti -n bkindwscf task -- /bin/bash -c "CLUSTER_NAME=$CLUSTER_NAME make buildir login" || true
 
+echo "source build$CLUSTER_NAME/.envrc" > .bashrc
+kubectl cp .bashrc bkindwscf/task:/bkindwscf/
+rm -rf .bashrc
+
 echo
 echo "@@@@@@@@@@@@@@"
-echo "Run: source build$CLUSTER_NAME/.envrc"
+echo "Executing into the persistent pod"
+echo "You can already use 'cf' and 'kubectl'"
+echo "Note: After you are done, you need to remove it explictly with: kubectl delete pod -n bkindwscf task"
 echo "@@@@@@@@@@@@@@"
 echo
-exec kubectl exec -ti task -n bkindwscf -- /bin/bash
+
+exec kubectl exec -ti task -n bkindwscf -- /bin/bash -l -c "source build$CLUSTER_NAME/.envrc && /bin/bash"
