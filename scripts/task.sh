021085b024cfc2b26933a32cb7d0aff90624ee16
Ettore Di Giacinto <edigiacinto@suse.com>
2019-09-26 12:15:06 +0200

    Make the task target source the env before running any job

--- scripts/task.sh
+++ scripts/task.sh
@@ -3,11 +3,10 @@
 . scripts/include/common.sh
 . .envrc
 
-set -exuo pipefail
+set -euo pipefail
 
 kubectl create namespace bkindwscf || true
-kubectl delete -f ../kube/task.yaml || true
-kubectl create -f ../kube/task.yaml 
+kubectl create -f ../kube/task.yaml || true
 
 bash ../scripts/wait_ns.sh bkindwscf
 
@@ -16,4 +15,13 @@ kubectl cp $TASK_SCRIPT bkindwscf/task:/bkindwscf/build$CLUSTER_NAME/
 
 kubectl exec -ti -n bkindwscf task -- bash -c "CLUSTER_NAME=$CLUSTER_NAME make buildir login"
 
-exit $(kubectl exec -ti task -n bkindwscf /bkindwscf/build$CLUSTER_NAME/$(basename $TASK_SCRIPT))
+
+echo
+echo "@@@@@@@@@@@@@@"
+echo "Running $TASK_SCRIPT in /bkindwscf/build$CLUSTER_NAME inside the task pod (in bkindwscf namespace)"
+echo "@@@@@@@@@@@@@@"
+echo
+
+kubectl exec -ti task -n bkindwscf -- /bin/bash -l -c "source build$CLUSTER_NAME/.envrc && /bkindwscf/build$CLUSTER_NAME/$(basename $TASK_SCRIPT)"
+status=$?
+exit $status
