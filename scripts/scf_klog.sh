893c2f76070db19076758382d1d2f7a1b8be7ce4
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-09-30 15:05:00 +0200

    Remove check script from s/scf_klog.sh, already in scf_precheck.sh

--- scripts/scf_klog.sh
+++ scripts/scf_klog.sh
@@ -5,8 +5,6 @@
 
 set -Eeuxo pipefail
 
-curl -Lo kube-ready-state-check.sh "$SCF_REPO"/raw/"$SCF_BRANCH"/bin/dev/kube-ready-state-check.sh
-
 curl -Lo klog.sh "$SCF_REPO"/raw/"$SCF_BRANCH"/container-host-files/opt/scf/bin/klog.sh
 chmod +x klog.sh
 mv klog.sh bin/
