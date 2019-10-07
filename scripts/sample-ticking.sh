0f62a7c4cc437f8b97fb5fd38630201dea3ea3f8
Ettore Di Giacinto <edigiacinto@suse.com>
2019-10-07 08:59:49 +0200

    Add sample-ticking target

---
+++ scripts/sample-ticking.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex 
+
+. scripts/include/common.sh
+. .envrc
+
+SAMPLE_APP_REPO="${SAMPLE_APP_REPO:-https://github.com/cloudfoundry-samples/cf-sample-app-nodejs}"
+
+# Don't touch original copy
+cp -rfv ../contrib/samples/ticking_app ./
+
+pushd ticking_app
+
+go build -o log_producing_app main.go
+
+if [ -n "$EKCP_PROXY" ]; then
+    export https_proxy=socks5://127.0.0.1:${KUBEPROXY_PORT}
+fi
+
+cf push
