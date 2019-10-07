99c5c4c8b319e1984c9e274d48d2ab5d7ea5d8fc
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-10-07 17:41:18 +0200

    Make deletion of metrics-server in scf_clean.sh idempotent

--- scripts/scf/clean.sh
+++ scripts/scf/clean.sh
@@ -25,7 +25,9 @@ if [[ "$ENABLE_EIRINI" == true ]] ; then
     if kubectl get namespaces 2>/dev/null | grep -qi eirini ; then
         kubectl delete namespace eirini
     fi
-    helm del --purge metrics-server
+    if helm ls 2>/dev/null | grep -qi metrics-server ; then
+        helm del --purge metrics-server
+    fi
 fi
 
 rm -rf scf-config-values.yaml chart.zip helm kube "$CF_HOME"/.cf
