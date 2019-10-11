204351c74c10435ce77b9829875dd7036a87ece5
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-10-11 14:33:25 +0200

    Rename caasp4os/deploy.sh paths to use backend/caasp4os

--- backend/caasp4os/deploy.sh
+++ backend/caasp4os/deploy.sh
@@ -74,11 +74,11 @@ sed -e "s%#~placeholder_stack~#%$(escapeSubst "$STACK")%g" \
     -e "s%#~placeholder_caasp_repo~#%$(escapeSubst "$CAASP_REPO")%g" \
     -e "s%#~placeholder_sshkey~#%$(escapeSubst "$SSHKEY")%g" \
     -e "s%#~placeholder_caasp_pattern~#%$(escapeSubst "$CAASP_PATTERN")%g" \
-    "$ROOT_DIR"/scripts/caasp4/terraform-os/terraform.tfvars.skel > \
+    "$ROOT_DIR"/backend/caasp4os/terraform-os/terraform.tfvars.skel > \
     deployment/terraform.tfvars
 sed -i '/\"\${openstack_networking_secgroup_v2\.common\.name}\",/a \ \ \ \ "\${openstack_compute_secgroup_v2.secgroup_cap.name}",' \
     deployment/worker-instance.tf
-cp -r "$ROOT_DIR"/scripts/caasp4/terraform-os/* deployment/
+cp -r "$ROOT_DIR"/backend/caasp4os/terraform-os/* deployment/
 
 pushd deployment
 
