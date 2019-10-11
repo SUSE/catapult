e14b0f4d6a6524b0f882a62688d6e8982569d04a
Ettore Di Giacinto <edigiacinto@suse.com>
2019-10-11 17:54:30 +0200

    Get container_ip from docker in kind docker_kubeconfig

--- backend/kind/docker_kubeconfig.sh
+++ backend/kind/docker_kubeconfig.sh
@@ -2,8 +2,10 @@
 set -ex
 
 . ../../include/common.sh
+. .envrc
 
-public_ip=$(kubectl get configmap -n kube-system cap-values -o json | jq -r '.data["public-ip"]')
+export container_id=$(docker ps -f "name=${CLUSTER_NAME}-control-plane" -q)
+export container_ip=$(docker inspect $container_id | jq -r .[0].NetworkSettings.Networks.bridge.IPAddress)
 
 # Tweaks the kubeconfig so it can be called from the docker container network (in case the cluster is created with dind)
-sed -i "s/localhost.*/${public_ip}:6443/" kubeconfig
+sed -i "s/localhost.*/${container_ip}:6443/" kubeconfig
