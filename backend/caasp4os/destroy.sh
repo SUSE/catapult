a3e6282e7f6b1beb7121f8d6d0cdca800daaa560
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-10-11 15:12:28 +0200

    Fix paths on caasp4os backend

--- backend/caasp4os/destroy.sh
+++ backend/caasp4os/destroy.sh
@@ -8,13 +8,14 @@
 # - Key on the ssh keyring
 
 . "$( dirname "${BASH_SOURCE[0]}" )"/caasp4os.sh
+. "$( dirname "${BASH_SOURCE[0]}" )"/lib/skuba.sh
 . ../../include/common.sh
 . .envrc
 
 set -exuo pipefail
 
 
-if [ -d "$ROOT_DIR"/"$BUILD_DIR" ]; then
+if [ -d "$BUILD_DIR" ]; then
     if [[ ! -v OS_PASSWORD ]]; then
         echo ">>> Missing openstack credentials" && exit 1
     fi
