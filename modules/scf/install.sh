1a8bb5a4855aa7941db38881e9e3534da32baaa9
Ettore Di Giacinto <edigiacinto@suse.com>
2019-10-14 10:06:34 +0200

    Add catapult integration tests
    Reduce sleep time to prevent travis timeout.
    Do not deploy SCF by default, takes too long for CI.

--- modules/scf/install.sh
+++ modules/scf/install.sh
@@ -65,7 +65,7 @@ elif [ "${SCF_OPERATOR}" == "true" ]; then
     --namespace scf \
     --values scf-config-values.yaml
 
-    sleep 900
+    sleep 360
 else
 
     helm install helm/cf --name susecf-scf --namespace scf \
