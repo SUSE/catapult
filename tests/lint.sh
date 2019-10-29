0507ee8c4fe7d09973b7b08ef6f5b9b776f8d6c9
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-10-29 16:35:21 +0100

    Reimplement tests/lint.sh posix compliant

--- tests/lint.sh
+++ tests/lint.sh
@@ -2,19 +2,13 @@
 
 . ../include/common.sh
 
-# use extended regexp patterns for matching:
-shopt -s extglob
-# remove empty matches when search-on-depth, instead of failing:
-shopt -s nullglob
-# match **/ as all dirs and subdirs:
-shopt -s globstar
-
 info "Linting shell scripts" && debug_mode
-SH_FILES=$(ls "$ROOT_DIR"/**/*.{sh,ksh,bash} | grep -v "shunit2" | grep -v "build*")
+SH_FILES=$(find "$ROOT_DIR" -type f -name '*.sh' -o -name '*.ksh' -o -name '*.bash' | grep -v "shunit2" | grep -v "build*")
+
 shellcheck --severity=error $SH_FILES || retcode=1
 
 info "Linting yamls" && debug_mode
-YML_FILES=$(ls "$ROOT_DIR"/**/*.{yaml,yml} | grep -v "shunit2" | grep -v "build*")
+YML_FILES=$(find "$ROOT_DIR" -type f -name '*.yaml' -o -name '*.yml' | grep -v "shunit2" | grep -v "build*")
 yamllint -d relaxed --strict $YML_FILES || retcode=1
 
 if [[ $retcode == 1 ]] ; then
