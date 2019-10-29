33f24879f9611db3354f9d0c77e9d0159a43c3f8
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-10-29 16:43:13 +0100

    Fix grep on tests/lint.sh

--- tests/lint.sh
+++ tests/lint.sh
@@ -3,12 +3,12 @@
 . ../include/common.sh
 
 info "Linting shell scripts" && debug_mode
-SH_FILES=$(find "$ROOT_DIR" -type f -name '*.sh' -o -name '*.ksh' -o -name '*.bash' | grep -v "shunit2" | grep -v "build*")
+SH_FILES=$(find "$ROOT_DIR" -type f -name '*.sh' -o -name '*.ksh' -o -name '*.bash' | grep -v "shunit2" | grep -v '^build')
 
 shellcheck --severity=error $SH_FILES || retcode=1
 
 info "Linting yamls" && debug_mode
-YML_FILES=$(find "$ROOT_DIR" -type f -name '*.yaml' -o -name '*.yml' | grep -v "shunit2" | grep -v "build*")
+YML_FILES=$(find "$ROOT_DIR" -type f -name '*.yaml' -o -name '*.yml' | grep -v "shunit2" | grep -v '^build')
 yamllint -d relaxed --strict $YML_FILES || retcode=1
 
 if [[ $retcode == 1 ]] ; then
