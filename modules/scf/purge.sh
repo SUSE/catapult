925c72d6caf9aff7cc9259a437f4827dab1665e4
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-10-29 17:29:34 +0100

    Fix shell script errors from linting

--- modules/scf/purge.sh
+++ modules/scf/purge.sh
@@ -11,9 +11,9 @@ info "Purging all apps, buildpacks and services from the CF instance"
 for app in $(cf apps | gawk '{print $1}'); do cf delete -f $app; done
 
 # Delete all buildpacks (in case there are leftovers)
-for buildpack in $(cf buildpacks | tail -n +4 | gawk '{print $1} do'); cf delete-buildpack -f $buildpack; done
+for buildpack in $(cf buildpacks | tail -n +4 | gawk '{print $1} do'); do cf delete-buildpack -f $buildpack; done
 
-# Delete all services 
+# Delete all services
 for service in $(cf services | tail -n +4 | gawk '{print $1}'); do cf delete-service -f $service; done
 
 ok "Purge completed"
