be08b8d4529ac10e7c4967c67be14fc61c70d388
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-11-11 13:24:50 +0100

    Add unit tests for STORAGECLASS option for scf-gen-config

--- tests/tests_catapult.sh
+++ tests/tests_catapult.sh
@@ -34,7 +34,7 @@ testBuilddir() {
 testConfig() {
   rm -rf buildtest
   make buildir
-  DEFAULT_STACK=sle CLUSTER_PASSWORD=test123 make scf-gen-config
+  DEFAULT_STACK=sle CLUSTER_PASSWORD=test123 STORAGECLASS=our-storage make scf-gen-config
   assertTrue 'create buildir' "[ -d $ROOT_DIR/buildtest ]"
   assertTrue 'create config values' "[ -e $ROOT_DIR/buildtest/scf-config-values.yaml ]"
 
@@ -43,6 +43,9 @@ testConfig() {
   assertContains 'generates correctly KUBE_CSR_AUTO_APPROVAL' "$VALUES_FILE" "KUBE_CSR_AUTO_APPROVAL: true"
   assertContains 'generates correctly DEFAULT_STACK' "$VALUES_FILE" "DEFAULT_STACK: \"sle\""
   assertContains 'generates correctly GARDEN_ROOTFS_DRIVER' "$VALUES_FILE" "GARDEN_ROOTFS_DRIVER: \"btrfs\""
+  assertContains 'generates correctly STORAGECLASS (1)' "$VALUES_FILE" "kube_storage_class: \"our-storage\""
+  assertContains 'generates correctly STORAGECLASS (2)' "$VALUES_FILE" "persistent: \"our-storage\""
+  assertContains 'generates correctly STORAGECLASS (3)' "$VALUES_FILE" "shared: \"our-storage\""
 }
 
 # Tests backend switch
