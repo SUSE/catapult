a29a76ec2f0f86a329ff1d5da576914bafea9d0c
Tim Hardeck <thardeck@suse.de>
2019-12-04 15:26:45 +0100

    Fix download of kind
    Starting with version 0.3.0 kind has a v in front of the version.

--- backend/kind/deps.sh
+++ backend/kind/deps.sh
@@ -10,7 +10,11 @@ else
   export KIND_OS_TYPE="${KIND_OS_TYPE:-kind-linux-amd64}"
 fi
 
-curl -Lo kind https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/${KIND_OS_TYPE}
+if [[ $KIND_VERSION =~ ^0\.2\.[0-9]$ ]]; then
+  curl -Lo kind https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/${KIND_OS_TYPE}
+else
+  curl -Lo kind https://github.com/kubernetes-sigs/kind/releases/download/v${KIND_VERSION}/${KIND_OS_TYPE}
+fi
 chmod +x kind
 mv kind bin/kind
 
