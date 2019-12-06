8b0bbcddb31adf82f6979e8a6b79d0df3047cb58
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-12-06 16:33:57 +0100

    Fix tests/kubesmokes.sh by exporting SMOKES_REPO
    SMOKES_REPO is used by kube/smokes/pod.yaml.erb

--- modules/tests/kubesmokes.sh
+++ modules/tests/kubesmokes.sh
@@ -17,6 +17,7 @@ info
 kubectl create namespace catapult || true
 kubectl delete pod smokes -n catapult || true
 
+export SMOKES_REPO=$SMOKES_REPO
 pod_definition=$(erb "$ROOT_DIR"/kube/smokes/pod.yaml.erb)
 cat <<EOF
 Will create this pod (if you see empty values, make sure you defined all the needed env variables):
