8bc9117d85698673024b1ee700074ab92de40363
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-12-11 14:58:39 +0100

    Do helm fetch when STRATOS_CHART=latest instead of using github release
    This makes sanity tests of stratos easier as we use the correct chart from
    kubernetes-charts.suse.com.

--- modules/stratos/chart.sh
+++ modules/stratos/chart.sh
@@ -4,28 +4,38 @@
 . ../../include/common.sh
 . .envrc
 
-if [ "$METRICS_CHART" = "latest" ]; then
-    warn "No stratos chart url given - using latest public release from GH"
-        STRATOS_CHART=$(curl -s https://api.github.com/repos/cloudfoundry/stratos/releases/latest | grep "browser_download_url.*zip" | cut -d : -f 2,3 | tr -d \" | tr -d " ")
-fi
-
-if echo "$STRATOS_CHART" | grep -q "http"; then
-    curl -L "$STRATOS_CHART" -o stratos-chart
-else
-    cp -rfv "$STRATOS_CHART" stratos-chart
-fi
-
 # remove old uncompressed chart
 rm -rf console
 
-if echo "$STRATOS_CHART" | grep -q "tgz"; then
-    tar -xvf stratos-chart -C ./
-else
-    unzip -o stratos-chart
-fi
-rm stratos-chart
+if [ "$STRATOS_CHART" = "latest" ]; then
+    warn "No stratos chart url given - using latest public release from kubernetes-charts.suse.com"
+    HELM_REPO="https://kubernetes-charts.suse.com/"
+    HELM_REPO_NAME="suse"
 
-# save STRATOS_CHART on cap-values configmap
-kubectl patch -n kube-system configmap cap-values -p $'data:\n stratos-chart: "'$STRATOS_CHART'"'
+    helm init --client-only
+    helm repo add "$HELM_REPO_NAME" $HELM_REPO
+    helm repo update
+    helm fetch "$HELM_REPO_NAME"/console
+    tar -xvf console-*
+    rm console-*.tgz
+    STRATOS_CHART_NAME=$(cat console/values.yaml | grep consoleVersion | cut -d " " -f2)
+else
+    if echo "$STRATOS_CHART" | grep -q "http"; then
+        curl -L "$STRATOS_CHART" -o stratos-chart
+    else
+        cp -rfv "$STRATOS_CHART" stratos-chart
+    fi
+
+    if echo "$STRATOS_CHART" | grep -q "tgz"; then
+        tar -xvf stratos-chart -C ./
+    else
+        unzip -o stratos-chart
+    fi
+    rm stratos-chart
+    STRATOS_CHART_NAME="$STRATOS_CHART"
+fi
+
+# save STRATOS_CHART_NAME on cap-values configmap
+kubectl patch -n kube-system configmap cap-values -p $'data:\n stratos-chart: "'$STRATOS_CHART_NAME'"'
 
 ok "Stratos chart uncompressed"
