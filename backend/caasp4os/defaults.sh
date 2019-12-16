1e78e052de4ecb99221cc09fa1f72cbc590f08e0
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-12-16 16:05:01 +0100

    Use CAASP_VER=update as default

--- backend/caasp4os/defaults.sh
+++ backend/caasp4os/defaults.sh
@@ -3,5 +3,5 @@
 # Caasp4os options
 ##################
 
-CAASP_VER=${CAASP_VER:-"product"}
+CAASP_VER=${CAASP_VER:-"update"} # devel, staging, update, product
 STACK=${STACK:-"$(whoami)-caasp4-${CAASP_VER::3}-$CLUSTER_NAME"}
