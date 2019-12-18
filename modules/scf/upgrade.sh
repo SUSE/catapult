f0e13bb1382b5d9f724b22f1629fe4f8dc754048
Ettore Di Giacinto <edigiacinto@suse.com>
2019-12-18 10:43:47 +0100

    Support kubecf upgrades

--- modules/scf/upgrade.sh
+++ modules/scf/upgrade.sh
@@ -10,23 +10,32 @@ if [ -n "$SCF_CHART" ]; then
     kubectl patch -n kube-system configmap cap-values -p $'data:\n chart: "'$SCF_CHART'"'
 fi
 
-SECRET=$(kubectl get pods --namespace uaa \
--o jsonpath='{.items[?(.metadata.name=="uaa-0")].spec.containers[?(.name=="uaa")].env[?(.name=="INTERNAL_CA_CERT")].valueFrom.secretKeyRef.name}')
+if [ "${SCF_OPERATOR}" == "true" ]; then
 
-CA_CERT="$(kubectl get secret "$SECRET" --namespace uaa \
--o jsonpath="{.data['internal-ca-cert']}" | base64 --decode -)"
+    helm upgrade --recreate-pods susecf-scf kubecf/ --values scf-config-values.yaml
+    sleep 10
 
-if [ "$UAA_UPGRADE" == true ]; then
+else
 
-    helm upgrade --recreate-pods susecf-uaa helm/uaa/ --values scf-config-values.yaml \
+    SECRET=$(kubectl get pods --namespace uaa \
+    -o jsonpath='{.items[?(.metadata.name=="uaa-0")].spec.containers[?(.name=="uaa")].env[?(.name=="INTERNAL_CA_CERT")].valueFrom.secretKeyRef.name}')
+
+    CA_CERT="$(kubectl get secret "$SECRET" --namespace uaa \
+    -o jsonpath="{.data['internal-ca-cert']}" | base64 --decode -)"
+
+    if [ "$UAA_UPGRADE" == true ]; then
+
+        helm upgrade --recreate-pods susecf-uaa helm/uaa/ --values scf-config-values.yaml \
+        --set "secrets.UAA_CA_CERT=${CA_CERT}"
+
+        bash "$ROOT_DIR"/include/wait_ns.sh uaa
+
+    fi
+
+
+    helm upgrade --recreate-pods susecf-scf helm/cf/ --values scf-config-values.yaml \
     --set "secrets.UAA_CA_CERT=${CA_CERT}"
-
-    bash "$ROOT_DIR"/include/wait_ns.sh uaa
-
 fi
-
-helm upgrade --recreate-pods susecf-scf helm/cf/ --values scf-config-values.yaml \
---set "secrets.UAA_CA_CERT=${CA_CERT}"
 
 bash "$ROOT_DIR"/include/wait_ns.sh scf
 
