9e209b69a87a6c4140562cf0f60d7d3b8e8ec205
Ettore Di Giacinto <edigiacinto@suse.com>
2020-01-03 15:49:00 +0100

    Add starlark svc to add the starlark-convert drone plugin

--- modules/extra/drone.sh
+++ modules/extra/drone.sh
@@ -7,7 +7,7 @@
 
 info "Deploying drone from the helm charts - be sure to have deployed gitea first, as drone will use gitea to run your pipeline against"
 domain=$(kubectl get configmap -n kube-system cap-values -o json | jq -r '.data["domain"]')
-
+plugin_secret="$(openssl rand -hex 16)"
 warn "!!!! It needs a gitea deployment first !!!!!"
 
 info "If you didn't already: "
@@ -20,6 +20,45 @@ helm delete --purge drone || true
 
 kubectl delete secret -n drone --all || true
 kubectl delete pvc -n drone --all || true
+kubectl delete namespace drone || true
+kubectl delete svc -n drone --all || true
+kubectl delete pod -n drone --all || true
+kubectl create namespace drone
+cat <<EOF | kubectl apply -f -
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: starlark
+  namespace: drone
+  labels:
+    app: starlark
+spec:
+  containers:
+    - name: starlark
+      image: drone/drone-convert-starlark
+      env:
+      - name: DRONE_SECRET
+        value: "$plugin_secret"
+      - name: DRONE_DEBUG
+        value: "true"
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: starlark
+  namespace: drone
+spec:
+  selector:
+    app: starlark
+  ports:
+    - protocol: TCP
+      port: 3000
+      targetPort: 3000
+
+EOF
+
+starlark_svc=$(kubectl get svc -n drone starlark -o jsonpath="{.spec.clusterIP}")
 
 helm install --name drone --namespace drone stable/drone
 
@@ -32,6 +71,8 @@ helm upgrade drone \
   --set "service.nodePort=32011" --set 'sourceControl.provider=gitea' \
   --set "sourceControl.gitea.clientID=${DRONE_CLIENT_ID}" \
   --set "sourceControl.gitea.server=http://${domain}:30080" \
+  --set "server.env.DRONE_CONVERT_PLUGIN_ENDPOINT=http://${starlark_svc}:3000" \
+  --set "server.env.DRONE_CONVERT_PLUGIN_SECRET=$plugin_secret" \
   --set 'sourceControl.secret=drone-server-secrets' --set "server.host=${domain}:32011" \
   stable/drone
 
