f735a4d288b3a83a1043ddb1802defc7664c18de
Ettore Di Giacinto <edigiacinto@suse.com>
2020-01-03 11:28:07 +0100

    Add instructions to connect to gitea with ekcp backend

--- modules/extra/gitea.sh
+++ modules/extra/gitea.sh
@@ -32,12 +32,22 @@ EOF
 
 helm install --values gitea-config-values.yaml --name gitea --namespace gittea jfelten/gitea
 bash "$ROOT_DIR"/include/wait_ns.sh gittea
-info "Gitea installed: http://${domain}:30080 ssh://git@${domain}:30222"
+
+if [ "$BACKEND" == "ekcp" ]; then
+  PODNAME=$(kubectl get pods -n gittea -l app=gitea-gitea -o jsonpath="{.items[0].metadata.name}")
+  info "Inside the cluster, gitea is reachable at http://${domain}:30080 ssh://git@${domain}:30222"
+  info "To access it from your local machine, run:"
+  info "for http access (to local http://127.0.0.1:8080): kubectl port-forward --namespace gittea $PODNAME 8080:3000"
+  info "for ssh access (to local ssh://git@127.0.0.1:2222): kubectl port-forward --namespace gittea $PODNAME 2222:22"
+else
+  info "Gitea installed: http://${domain}:30080 ssh://git@${domain}:30222"
+fi
 
 info "Go to the gitea webui and finish the setup."
 warn "Mind to setup the SSH Server port to 30222 and the Gitea Base URL as http://${domain}:30080/"
 
+
 info "If you plan to use it with drone:"
 info "1. Go to http://${domain}:30080/user/settings/applications"
 info "2. Create a new application, name it drone, redirect uri is: http://${domain}:32011/login"
-info "Use those secrets for DRONE_CLIENT_ID and DRONE_CLIENT_SECRET"
+info "Use those secrets for DRONE_CLIENT_ID and DRONE_CLIENT_SECRET when running module-extra-drone"
