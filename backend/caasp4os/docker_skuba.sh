6747dac6a835b19e040b03948ab6a7088a3bfa51
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-01-08 16:38:37 +0100

    Fix make deps-caasp4os when CLUSTER_NAME default

--- backend/caasp4os/docker_skuba.sh
+++ backend/caasp4os/docker_skuba.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -Eeuo pipefail
-
 . ./defaults.sh
+
+set -Eeuo pipefail
 
 if [[ "$(docker images -q skuba/$CAASP_VER 2> /dev/null)" == "" ]]; then
 		make -C docker/skuba/ "$CAASP_VER"
