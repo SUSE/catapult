c18a79b5a130145d93bafc14d5a80ba8a3cdcb63
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-01-15 15:33:15 +0100

    Remove SAMPLE_APP_REPO from m/t/sample.sh, already on m/t/defaults.sh

--- modules/tests/sample.sh
+++ modules/tests/sample.sh
@@ -4,7 +4,6 @@
 . ../../include/common.sh
 . .envrc
 
-SAMPLE_APP_REPO="${SAMPLE_APP_REPO:-https://github.com/cloudfoundry-samples/cf-sample-app-nodejs}"
 
 [ ! -d "sample" ] && git clone --recurse-submodules "$SAMPLE_APP_REPO" sample
 
