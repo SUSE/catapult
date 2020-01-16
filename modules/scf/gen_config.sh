2580b077b0390220477fdf70bccaf51fcd0ceda5
Ettore Di Giacinto <edigiacinto@suse.com>
2020-01-16 11:25:03 +0100

    Fixups

--- modules/scf/gen_config.sh
+++ modules/scf/gen_config.sh
@@ -49,9 +49,9 @@ END_HEREDOC
 fi
 
 if [ "${SCF_OPERATOR}" == "true" ]; then
-$CLUSTER_IP_RANGE="$(kubectl cluster-info dump --output yaml | awk 'match($0, /service-cluster-ip-range=(.*)/, range) { print range[1] }' | uniq)"
+CLUSTER_IP_RANGE="$(kubectl cluster-info dump --output yaml | awk 'match($0, /service-cluster-ip-range=(.*)/, range) { print range[1] }' | uniq)"
 
-$CIDR="$(kubectl cluster-info dump --output yaml | awk 'match($0, /cluster-cidr=(.*)/, range) { print range[1] }')"
+CIDR="$(kubectl cluster-info dump --output yaml | awk 'match($0, /cluster-cidr=(.*)/, range) { print range[1] }')"
 
 if [ -z "$CLUSTER_IP_RANGE" ];
 then
