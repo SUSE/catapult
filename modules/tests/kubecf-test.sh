99999fad4c9494e3ccf69e4ab9b1223f4a2a5ba2
Ettore Di Giacinto <edigiacinto@suse.com>
2020-01-17 11:20:40 +0100

    Handle pod termination before we wait

--- modules/tests/kubecf-test.sh
+++ modules/tests/kubecf-test.sh
@@ -21,11 +21,11 @@ smoke_tests_pod_name() {
 
 # Wait for smoke-tests to start.
 wait_for_smoke_tests_pod() {
-  local timeout="900"
+  local timeout="300"
   until kubectl get pods --namespace "${KUBECF_NAMESPACE}" --output name 2> /dev/null | grep --quiet "smoke-tests" || [[ "$timeout" == "0" ]]; do sleep 1; timeout=$((timeout - 1)); done
   if [[ "${timeout}" == 0 ]]; then return 1; fi
   pod_name="$(smoke_tests_pod_name)"
-  until [[ "$(kubectl get "${pod_name}" --namespace "${KUBECF_NAMESPACE}" --output jsonpath='{.status.containerStatuses[?(@.name == "smoke-tests-smoke-tests")].state.running}' 2> /dev/null)" != "" ]] || [[ "$timeout" == "0" ]]; do sleep 1; timeout=$((timeout - 1)); done
+  until [[ "$(kubectl get "${pod_name}" --namespace "${KUBECF_NAMESPACE}" --output jsonpath='{.status.containerStatuses[?(@.name == "smoke-tests-smoke-tests")].state.running}' 2> /dev/null)" != "" ]] || [[ "$(kubectl get "${pod_name}" --namespace "${KUBECF_NAMESPACE}" --output jsonpath='{.status.containerStatuses[?(@.name == "smoke-tests-smoke-tests")].state.terminated}' 2> /dev/null)" != "" ]]  || [[ "$timeout" == "0" ]]; do sleep 1; timeout=$((timeout - 1)); done
   if [[ "${timeout}" == 0 ]]; then return 1; fi
   return 0
 }
@@ -40,7 +40,7 @@ if [ "${KUBECF_TEST_SUITE}" == "smokes" ]; then
 
     # Follow the logs. If the tests fail, the logs command will also fail.
     pod_name="$(smoke_tests_pod_name)"
-    kubectl logs --follow "${pod_name}" --namespace "${KUBECF_NAMESPACE}" --container smoke-tests-smoke-tests
+    kubectl attach "${pod_name}" --namespace "${KUBECF_NAMESPACE}" --container smoke-tests-smoke-tests ||:
 
     # Wait for the container to terminate and then exit the script with the container's exit code.
     jsonpath='{.status.containerStatuses[?(@.name == "smoke-tests-smoke-tests")].state.terminated.exitCode}'
