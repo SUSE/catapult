0ed10cabd4946f6937605efab921e1147f0e4354
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-01-31 16:10:49 +0100

    Migrate stratos/gen-config/sh to yamlpatch

--- modules/stratos/gen-config.sh
+++ modules/stratos/gen-config.sh
@@ -7,13 +7,31 @@ info "Generating stratos config values from scf values"
 
 cp scf-config-values.yaml scf-config-values-for-stratos.yaml
 
-cat <<HEREDOC_APPEND >> scf-config-values-for-stratos.yaml
+cat <<EOF > op.yml
+- op: add
+  path: /console
+  value:
+    service:
+      ingress:
+        enabled: true
+- op: replace
+  path: /kube/registry/hostname
+  value:
+    "${DOCKER_REGISTRY}"
+- op: replace
+  path: /kube/registry/username
+  value:
+    "${DOCKER_USERNAME}"
+- op: replace
+  path: /kube/registry/password
+  value:
+    "${DOCKER_PASSWORD}"
+- op: replace
+  path: /kube/organization
+  value:
+    "${DOCKER_ORG}"
+EOF
 
-# Appended for stratos:
-console:
-  service:
-    ingress:
-      enabled: true
-HEREDOC_APPEND
+yamlpatch op.yml scf-config-values-for-stratos.yaml
 
 ok "Stratos config values generated"
