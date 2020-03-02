352fdf9adc72aec9e4df21ce37cffcf86e70a3b0
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-03-02 11:12:42 +0100

    Fix bug when DEFAULT_STACK=from_chart and SCF_OPERATOR=true
    DEFAULT_STACK was never used when SCF_OPERATOR=true, but it was failing
    silently. Make it explicit and don't make it fail.
    
    Thank god for git bisect.

--- modules/scf/gen_config.sh
+++ modules/scf/gen_config.sh
@@ -6,9 +6,11 @@
 
 
 info "Generating SCF config values"
-if [ "${DEFAULT_STACK}" = "from_chart" ]; then
-    DEFAULT_STACK=$(helm inspect helm/cf/ | grep DEFAULT_STACK | sed  's~DEFAULT_STACK:~~g' | sed 's~"~~g' | sed 's~\s~~g')
-    export DEFAULT_STACK
+if [ "$SCF_OPERATOR" != true ]; then
+    if [ "${DEFAULT_STACK}" = "from_chart" ]; then
+        DEFAULT_STACK=$(helm inspect helm/cf/ | grep DEFAULT_STACK | sed  's~DEFAULT_STACK:~~g' | sed 's~"~~g' | sed 's~\s~~g')
+        export DEFAULT_STACK
+    fi
 fi
 
 domain=$(kubectl get configmap -n kube-system cap-values -o json | jq -r '.data["domain"]')
