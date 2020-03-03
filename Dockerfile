86e0a47a9516aa17b2c0ab042bb7feee9a3abc6f
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-03-03 16:32:56 +0100

    Install aws cli on catapult image, and don't download on backend eks

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,22 @@ RUN zypper in -y vim zsh tmux glibc-locale glibc-i18ndata python ruby python3 py
 
 RUN zypper ar https://download.opensuse.org/repositories/devel:/languages:/go/openSUSE_Factory/devel:languages:go.repo
 RUN zypper --gpg-auto-import-keys -n in --from=devel_languages_go go1.13
+
 RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && \
     chmod +x ./kubectl && \
     mv ./kubectl /usr/local/bin/kubectl
+
 RUN curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
 RUN mv cf /usr/local/bin && chmod +x /usr/local/bin/cf
+
+RUN zypper in -y python-xml
+RUN curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+RUN unzip awscli-bundle.zip
+RUN ./awscli-bundle/install
+RUN rm -rf awscli-bundle*
+RUN curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
+RUN chmod +x aws-iam-authenticator && mv aws-iam-authenticator bin/
+
 ADD . /catapult
 WORKDIR /catapult
 ENTRYPOINT [ "/usr/bin/make" ]
--- backend/eks/deps.sh
+++ backend/eks/deps.sh
@@ -13,14 +13,16 @@ fi
 curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/kubectl
 chmod +x kubectl && mv kubectl bin/
 
-mkdir -p .local
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip
-./awscli-bundle/install --install-dir="$(pwd)"/.local/ --bin-location="$(pwd)"/bin/aws
-rm -rf awscli-bundle*
+if ! which aws ; then
+    mkdir -p .local
+    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+    unzip awscli-bundle.zip
+    ./awscli-bundle/install --install-dir=$(pwd)/.local/ --bin-location=$(pwd)/bin/aws
+    rm -rf awscli-bundle*
 
-curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
-chmod +x aws-iam-authenticator && mv aws-iam-authenticator bin/
+    curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
+    chmod +x aws-iam-authenticator && mv aws-iam-authenticator bin/
+fi
 
 curl -o terraform.zip https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.9_linux_amd64.zip
 # curl -o terraform.zip https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
