55cd8b330bf01f62c9f15fe0572dbecaae5c0d4a
Ettore Di Giacinto <edigiacinto@suse.com>
2020-03-09 10:07:22 +0100

    Add back HELM_VERSION to the backends

--- backend/caasp4os/defaults.sh
+++ backend/caasp4os/defaults.sh
@@ -5,3 +5,4 @@
 
 CAASP_VER=${CAASP_VER:-"update"} # devel, staging, update, product
 KUBECTL_VERSION="${KUBECTL_VERSION:-v1.16.2}"
+HELM_VERSION="${HELM_VERSION:-v3.1.1}"
--- backend/eks/defaults.sh
+++ backend/eks/defaults.sh
@@ -7,4 +7,5 @@ EKS_LOCATION="${EKS_LOCATION:-us-west-2}"
 EKS_KEYPAIR="${EKS_KEYPAIR:-$(whoami)-terraform}"
 EKS_VERS="${EKS_VERS:-1.14}"
 EKS_CLUSTER_LABEL=${EKS_CLUSTER_LABEL:-\{key = \"$(whoami)-eks-cluster\"\}}
+HELM_VERSION="${HELM_VERSION:-v3.1.1}"
 # EKS_KUBECTL_VERSION not to be defined as the eks kubectl is hardcoded in deps.sh
