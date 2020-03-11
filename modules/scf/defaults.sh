f3b00370cfb40dc758818f8171c8875bb53f0a59
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-03-11 17:08:30 +0100

    Add comments and reorder m/scf/defaults.sh

--- modules/scf/defaults.sh
+++ modules/scf/defaults.sh
@@ -3,12 +3,15 @@
 # SCF options
 #############
 
+# scf-chart revelant:
+
 CHART_URL="${CHART_URL:-}" # FIXME deprecated, used in SCF_CHART
 SCF_CHART="${SCF_CHART:-$CHART_URL}"
 
 SCF_HELM_VERSION="${SCF_HELM_VERSION:-}"
-SCF_OPERATOR="${SCF_OPERATOR:-false}"
 OPERATOR_CHART_URL="${OPERATOR_CHART_URL:-latest}"
+
+# scf-gen-config relevant:
 
 GARDEN_ROOTFS_DRIVER="${GARDEN_ROOTFS_DRIVER:-overlay-xfs}"
 DIEGO_SIZING="${DIEGO_SIZING:-$SIZING}"
@@ -29,8 +32,13 @@ UAA_UPGRADE="${UAA_UPGRADE:-true}"
 OVERRIDE="${OVERRIDE:-}"
 CONFIG_OVERRIDE="${CONFIG_OVERRIDE:-$OVERRIDE}"
 
-SCF_LOCAL="${SCF_LOCAL:-}"
-
 SCF_TESTGROUP="${SCF_TESTGROUP:-false}"
 
+# scf-build relevant:
+
+SCF_LOCAL="${SCF_LOCAL:-}"
+
+# relevant to several:
+
+SCF_OPERATOR="${SCF_OPERATOR:-false}"
 HELM_VERSION="${HELM_VERSION:-v3.1.1}"
