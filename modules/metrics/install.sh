a69346f4390f179d941eb1cc311598188cba2947
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-03-16 11:59:44 +0100

    Use helm_install when needed, and create namespaces when helm3
    Helm 3 doesn't create namespaces, we need to do it.

--- modules/metrics/install.sh
+++ modules/metrics/install.sh
@@ -6,13 +6,16 @@
 
 info "Deploying stratos-metrics"
 
+if [[ "$HELM_VERSION" == v3* ]]; then
+    kubectl create namespace "stratos-metrics"
+fi
 helm_install susecf-metrics ./metrics \
-     --namespace metrics \
+     --namespace stratos-metrics \
      --values scf-config-values-for-metrics.yaml \
      --values stratos-metrics-values.yaml
 
 wait_ns metrics
 
-kubectl get service susecf-metrics-metrics-nginx --namespace metrics
+kubectl get service susecf-metrics-metrics-nginx --namespace stratos-metrics
 
 ok "Stratos-metrics deployed successfully"
