d3f98f87fc17abcfc882295307023a657abc27ed
Victor Cuadrado Juan <viccuad@users.noreply.github.com>
2020-04-06 11:01:26 +0200

    Remove doubled assignation of exit_code
    Co-Authored-By: Mark Yen <3977982+mook-as@users.noreply.github.com>

--- modules/tests/kubecf-test.sh
+++ modules/tests/kubecf-test.sh
@@ -96,7 +96,7 @@ kubectl logs -f "${pod_name}" --namespace "${KUBECF_NAMESPACE}" --container "$co
 
 # Wait for the container to terminate and then exit the script with the container's exit code.
 jsonpath='{.status.containerStatuses[?(@.name == "'"$container_name"'")].state.terminated.exitCode}'
-exit_code="$(kubectl get "${pod_name}" --namespace "${KUBECF_NAMESPACE}" --output "jsonpath=${jsonpath}")"
+exit_code=""
 while [[ -z "${exit_code}" ]]; do
     exit_code="$(kubectl get "${pod_name}" --namespace "${KUBECF_NAMESPACE}" --output "jsonpath=${jsonpath}")"
     sleep 1
