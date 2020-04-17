98a014b2396d02c928d4496ca57d4537f2e7d41b
Andreas Kupries <andreas.kupries@suse.com>
2020-04-17 12:00:26 -0700

    [jira#cap-1396][kubecf#177] Switch to eksctl delete over terraform destroy.
    This is all draft.

--- backend/eks/clean.sh
+++ backend/eks/clean.sh
@@ -8,13 +8,31 @@
 if [ -d "$BUILD_DIR" ]; then
     . .envrc
 
+    # TODO: Get this as terraform output
+    # Currently assumes an exact structure of the kubeconfig
+    eks_cluster_name="$(cat kubeconfig \
+                    | yq '.users[0].user.exec.args[2]' \
+                    2>/dev/null|head -n1)"
 
-    if [ -d "cap-terraform/eks" ]; then
-        pushd cap-terraform/eks || exit
-        terraform destroy -auto-approve
-        popd || exit
-        rm -rf cap-terraform
-    fi
+    # See above, having it as proper terraform output is better.
+    # if [ -d "cap-terraform/eks" ]; then
+    #     eks_cluster_name="$(cd cap-terraform/eks ; terraform output cluster_name)"
+    # fi
+
+    eksctl delete cluster --name "${eks_cluster_name}"
+
+    # Activate if eksctl is good
+    # if [ -d "cap-terraform/eks" ]; then
+    #     rm -rf cap-terraform
+    # fi
+
+    # Old code disabled
+    # if [ -d "cap-terraform/eks" ]; then
+    #     pushd cap-terraform/eks || exit
+    #     terraform destroy -auto-approve
+    #     popd || exit
+    #     rm -rf cap-terraform
+    # fi
 
     popd || exit
     rm -rf "$BUILD_DIR"
