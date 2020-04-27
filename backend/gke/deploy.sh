067f9e2cbfb81c1ccf33e8bfdd9c90640407ee2e
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-04-27 17:53:01 +0200

    Add GKE_DNSDOMAIN and consume it

--- backend/gke/deploy.sh
+++ backend/gke/deploy.sh
@@ -56,12 +56,11 @@ info "Configuring deployed GKE cluster…"
 ROOTFS=overlay-xfs
 # take first worker node as public ip:
 PUBLIC_IP="$(kubectl get nodes -o json | jq -r '.items[].status.addresses[] | select(.type == "InternalIP").address' | head -n 1)"
-DOMAIN="$PUBLIC_IP.$MAGICDNS"
 if ! kubectl get configmap -n kube-system 2>/dev/null | grep -qi cap-values; then
     kubectl create configmap -n kube-system cap-values \
             --from-literal=garden-rootfs-driver="${ROOTFS}" \
             --from-literal=public-ip="${PUBLIC_IP}" \
-            --from-literal=domain="${DOMAIN}" \
+            --from-literal=domain="${GKE_DNSDOMAIN}" \
             --from-literal=platform=gke
 fi
 
