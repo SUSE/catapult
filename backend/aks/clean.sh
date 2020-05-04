de8c1acc5a5070c6b447aa2f0788035917ca91ee
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-05-04 11:43:16 +0200

    Add aks/clean.sh for make clean

--- backend/aks/clean.sh
+++ backend/aks/clean.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+# Requires:
+# - azure credentials present
+
 . ../../include/common.sh
 
 if [ -d "$BUILD_DIR" ]; then
-    # TODO call terraform destroy when needed
+    . .envrc
+
+    if [ -d "cap-terraform/aks" ]; then
+        pushd cap-terraform/aks || exit
+        terraform destroy -auto-approve
+        popd || exit
+    fi
+
+    popd || exit
     rm -rf "$BUILD_DIR"
 fi
+
+ok "AKS cluster deleted successfully"
