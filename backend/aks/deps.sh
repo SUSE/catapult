4dd7f1368ef9f604c23d7554d2cd247a394f7c34
Christian Hueller <chuller@suse.com>
2020-05-06 14:34:04 +0200

    Adjust the order of answers to the Azure installer so it works

--- backend/aks/deps.sh
+++ backend/aks/deps.sh
@@ -12,7 +12,7 @@ azclipath=bin/az
 if [ ! -e "$azclipath" ]; then
     # needs gcc libffi-devel python3-devel libopenssl-devel
     curl -o install.py https://azurecliprod.blob.core.windows.net/install.py && \
-        printf "$(pwd)/.lib/azure-cli\n$(pwd)/bin\ny\n$(pwd)/.envrc\n" | python3 ./install.py && \
+	printf "y\n$(pwd)/.lib/azure-cli\n$(pwd)/bin\nY\n$(pwd)/.envrc\n " | python3 ./install.py && \
         rm ./install.py
 fi
 
