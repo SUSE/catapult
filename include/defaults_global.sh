43b7d91afa6fa3d763840ac622a9e59ee750e5a4
Victor Cuadrado Juan <viccuad@users.noreply.github.com>
2020-06-08 10:15:54 +0200

    Update defaults_global.sh

--- include/defaults_global.sh
+++ include/defaults_global.sh
@@ -8,7 +8,7 @@ export DOCKER_ORG="${DOCKER_ORG:-cap}"
 
 export DEFAULT_STACK="${DEFAULT_STACK:-from_chart}" # from_chart, sle15, sle12, cfslinuxfs2, cfslinuxfs3
 export MAGICDNS="${MAGICDNS:-omg.howdoi.website}"
-export ENABLE_EIRINI="${ENABLE_EIRINI:-true}"
+export ENABLE_EIRINI="${ENABLE_EIRINI:-false}"
 export EKCP_PROXY="${EKCP_PROXY:-}"
 export KUBEPROXY_PORT="${KUBEPROXY_PORT:-2224}"
 export QUIET_OUTPUT="${QUIET_OUTPUT:-false}"
