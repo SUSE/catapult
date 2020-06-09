8b6a6b1d3c5c4a2cba6857077957f43c585d136b
Bikramdeep Singh <bikramdeep.singh@suse.com>
2020-06-09 12:52:23 -0700

    adding changes for eks deployment.

--- backend/eks/defaults.sh
+++ backend/eks/defaults.sh
@@ -3,12 +3,22 @@
 # EKS options
 #############
 
+EKS_CLUSTER_NAME="${EKS_CLUSTER_NAME:-$(whoami)-cap}"
 EKS_LOCATION="${EKS_LOCATION:-us-west-2}"
 EKS_KEYPAIR="${EKS_KEYPAIR:-$(whoami)-terraform}"
 EKS_VERS="${EKS_VERS:-1.14}"
 EKS_CLUSTER_LABEL=${EKS_CLUSTER_LABEL:-\{key = \"$(whoami)-eks-cluster\"\}}
 HELM_VERSION="${HELM_VERSION:-v3.1.1}"
-# EKS_KUBECTL_VERSION not to be defined as the eks kubectl is hardcoded in deps.sh
+
+EKS_HOSTED_ZONE_NAME="${EKS_HOSTED_ZONE_NAME:-qa.aws.howdoi.website}"
+EKS_DOMAIN="${DOMAIN:-${EKS_CLUSTER_NAME}.${EKS_HOSTED_ZONE_NAME}}"
+EKS_DEPLOYER_ROLE_ARN="${EKS_DEPLOYER_ROLE_ARN:-}"
+EKS_CLUSTER_ROLE_NAME="${EKS_CLUSTER_ROLE_NAME:-}"
+EKS_CLUSTER_ROLE_ARN="${EKS_CLUSTER_ROLE_ARN:-}"
+EKS_WORKER_NODE_ROLE_NAME="${EKS_WORKER_NODE_ROLE_NAME:-}"
+EKS_WORKER_NODE_ROLE_ARN="${EKS_WORKER_NODE_ROLE_ARN:-}"
+
+KUBE_AUTHORIZED_ROLE_ARN="${KUBE_AUTHORIZED_ROLE_ARN:-}"
 
 # Settings for terraform state save/restore
 #
@@ -19,8 +29,3 @@ TF_KEY="${TF_KEY:-}"
 # TF_KEY is empty (default, see above).
 TF_BUCKET="${TF_BUCKET:-cap-ci-tf}"
 TF_REGION="${TF_REGION:-${EKS_LOCATION}}"
-
-# DNS information (Route53). No defaults. Required.
-#EKS_ZONE_ID
-#EKS_ZONE_NAME
-#EKS_ZONE_POLICY
