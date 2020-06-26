70be063a158e989a8aaf06d9eaed86767e078be3
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-06-26 12:10:34 +0200

    Remove dependencies on GKE_CLUSTER_NAME,GKE_CLUSTER_ZONE from gke/kubeconfig.sh
    Importing a kubeconfig should always only depend on the kubeconfig, and general
    public cloud credentials.
    
    It should not depend on specifics of the cluster to be
    imported (GKE_CLUSTER_NAME or GKE_CLUSTER_ZONE). Because when importing just
    from a kubeconfig, one doesn't know any of those.
    
    Depending on specifics such as those breaks the case of importing your own
    kubeconfigs from the pool into the CI, since it's impossible for the CI to know
    the name of the cluster you are importing (nor it needs it anyways).
    
    Hence, this removes them.
    
    Tested by importing a kubeconfig from cf-ci-pools, and seeing that it works,
    `kubectl` work, and everything is fine.

--- backend/gke/kubeconfig.sh
+++ backend/gke/kubeconfig.sh
@@ -4,24 +4,8 @@
 . ../../include/common.sh
 . .envrc
 
-# KUBECFG can be a kubeconfig file or a cluster reference file for local deployments
-# KUBECFG has to be a kubeclusterreference for CI usage, check gke/deploy.sh for format
 if [ ! -f "$KUBECFG" ]; then
     err "No KUBECFG given - you need to pass one!"
-    exit 1
-fi
-
-if [[ $(yq r ${KUBECFG} kind) == "ClusterReference" ]]; then
-    # Process kubeclusterreference file
-    echo "Using kubeclusterreference ..."
-    GKE_CLUSTER_NAME="$(yq r ${KUBECFG} cluster-name)"
-    GKE_CLUSTER_ZONE="$(yq r ${KUBECFG} cluster-zone)"
-    GKE_PROJECT="$(yq r ${KUBECFG} project)"
-    export GKE_CLUSTER_NAME GKE_CLUSTER_ZONE GKE_PROJECT
-elif [[ $(yq r ${KUBECFG} kind) == "Config" ]]; then
-    echo "Using kubeconfig ..."
-else
-    echo "Please check your KUBECFG"
     exit 1
 fi
 
@@ -32,7 +16,8 @@ gcloud auth activate-service-account --project "$GKE_PROJECT" --key-file "$GKE_C
 if [[ $(gcloud auth list  --format="value(account)" | wc -l ) -le 0 ]]; then
     err "GKE_CRED_JSON creds don't authenticate, aborting" && exit 1
 fi
-gcloud container clusters get-credentials ${GKE_CLUSTER_NAME} --zone ${GKE_CLUSTER_ZONE}
+
+cp "$KUBECFG" kubeconfig
 
 # kubeconfig gets hardcoded paths for gcloud bin, reevaluate them:
 gcloud_path="$(which gcloud)"
