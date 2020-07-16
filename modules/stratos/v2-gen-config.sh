4a162518d93f0f624579891fcc28f880cf38d647
André Duffeck <aduffeck@suse.com>
2020-07-16 15:25:18 +0200

    Apply stratos patches against stratos yaml file
    Signed-off-by: Víctor Cuadrado <vcuadradojuan@suse.de>

--- modules/stratos/v2-gen-config.sh
+++ modules/stratos/v2-gen-config.sh
@@ -8,6 +8,7 @@ info "Generating stratos config values from scf values"
 cp scf-config-values.yaml stratos-config-values.yaml
 for patch in "$ROOT_DIR"/modules/stratos/patches/*.yaml; do
     selective-merge -d kubecf=scf-config-values.yaml \
+                    -d stratos=stratos-config-values.yaml \
                     -p "$patch" \
                     > stratos-config-values_temp.yaml
     mv stratos-config-values_temp.yaml stratos-config-values.yaml
