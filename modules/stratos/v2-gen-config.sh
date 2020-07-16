600987f451113591f96296d87cbf9278c7c66b66
André Duffeck <aduffeck@suse.com>
2020-07-16 14:53:39 +0200

    Fix iteration of patches in modules/stratos/v2-gen-config.sh
    Signed-off-by: Víctor Cuadrado <vcuadradojuan@suse.de>

--- modules/stratos/v2-gen-config.sh
+++ modules/stratos/v2-gen-config.sh
@@ -9,7 +9,8 @@ cp scf-config-values.yaml scf-config-values-for-stratos.yaml
 for patch in "$ROOT_DIR"/modules/stratos/patches/*.yaml; do
     selective-merge -d kubecf=scf-config-values-for-stratos.yaml \
                     -p "$patch" \
-                    > scf-config-values-for-stratos.yaml
+                    > scf-config-values-for-stratos_temp.yaml
+    mv scf-config-values-for-stratos_temp.yaml scf-config-values-for-stratos.yaml
 done
 
 ok "Stratos config values generated"
