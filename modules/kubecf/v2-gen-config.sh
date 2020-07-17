ef1ba1b46d93e9a5e9a9b3ff3a1bd6d9fc02bee7
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-07-17 14:39:17 +0200

    Add new kubecf patches. Disable the override one for now

--- modules/kubecf/v2-gen-config.sh
+++ modules/kubecf/v2-gen-config.sh
@@ -6,8 +6,29 @@
 
 info "Generating KubeCF config values"
 
+# generate all old-school env vars:
+
+kubectl patch -n kube-system configmap cap-values -p $'data:\n services: "'$SCF_SERVICES'"'
+export services="$SCF_SERVICES"
+export domain=$(kubectl get configmap -n kube-system cap-values -o json | jq -r '.data["domain"]')
+echo "$domain"
+export public_ip=$(kubectl get configmap -n kube-system cap-values -o json | jq -r '.data["public-ip"]')
+array_external_ips=()
+while IFS='' read -r line; do array_external_ips+=("$line");
+done < <(kubectl get nodes -o json | jq -r '.items[].status.addresses[] | select(.type == "InternalIP").address')
+external_ips+="\"$public_ip\""
+for (( i=0; i < ${#array_external_ips[@]}; i++ )); do
+    external_ips+=", \"${array_external_ips[$i]}\""
+done
+export external_ips
+
+# TODO the (( grab $env_var )) are quoting the vars, weirdly. It doesn't happen for stratos-config-values.yaml
+
+# compute the patches:
+
 cp values.yaml kubecf-config-values.yaml
 for patch in "$ROOT_DIR"/modules/kubecf/patches/*.yaml; do
+    echo "Applying patch $patch"
     selective-merge -d kubecf=kubecf-config-values.yaml \
                     -p "$patch" \
                     > kubecf-config-values_temp.yaml
@@ -15,50 +36,3 @@ for patch in "$ROOT_DIR"/modules/kubecf/patches/*.yaml; do
 done
 
 ok "KubeCF config values generated"
-exit
-
-########
-
-info "Generating KubeCF config values"
-
-kubectl patch -n kube-system configmap cap-values -p $'data:\n services: "'$SCF_SERVICES'"'
-services="$SCF_SERVICES"
-domain=$(kubectl get configmap -n kube-system cap-values -o json | jq -r '.data["domain"]')
-public_ip=$(kubectl get configmap -n kube-system cap-values -o json | jq -r '.data["public-ip"]')
-# array_external_ips=()
-# while IFS='' read -r line; do array_external_ips+=("$line");
-# done < <(kubectl get nodes -o json | jq -r '.items[].status.addresses[] | select(.type == "InternalIP").address')
-external_ips+="\"$public_ip\""
-# for (( i=0; i < ${#array_external_ips[@]}; i++ )); do
-# external_ips+=", \"${array_external_ips[$i]}\""
-# done
-
-if [ "$services" == ingress ]; then
-INGRESS_BLOCK="ingress:
-    enabled: true
-    tls:
-      crt: ~
-      key: ~
-    annotations: {}
-    labels: {}
-"
-else
-INGRESS_BLOCK=''
-fi
-
-cat > scf-config-values.yaml <<EOF
-system_domain: $domain
-
-features:
-  eirini:
-    enabled: ${ENABLE_EIRINI}
-  autoscaler:
-    enabled: ${AUTOSCALER}
-  ${INGRESS_BLOCK}
-
-high_availability: ${HA}
-EOF
-
-# TODO add functionality
-# if [ "${services}" == "lb" ]; then
-# fi
