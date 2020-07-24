8d55fcf77ec18b6c239927f91d8db509fdb1d84f
Christian Hueller <chuller@suse.com>
2020-07-24 11:44:35 +0200

    Make sure that defaults are used and sett ROOT_DIR correctly

--- modules/tests/stress-benchmark.sh
+++ modules/tests/stress-benchmark.sh
@@ -14,6 +14,8 @@ if [ -n "$EKCP_PROXY" ]; then
     export https_proxy=socks5://127.0.0.1:${KUBEPROXY_PORT}
 fi
 
-./start_benchmark.sh
 
-ok "stressbenchmark finished, results in $BUILD_DIR/$STRESS_FOLDER"
+export TEST_CONCURRENCY TEST_RATE TEST_DURATION
+ROOT_DIR=$BUILD_DIR/cf-benchmark-tools bash ./start_benchmark.sh
+
+ok "stressbenchmark finished, results in $BUILD_DIR/cf-benchmark-tools/bench*"
