4490981924d0f259b2a9ca57b29d6a05333ea220
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-10-08 17:35:53 +0200

    Set `services=hardcoded` on minikube

--- backend/minikube/prepare.sh
+++ backend/minikube/prepare.sh
@@ -11,7 +11,7 @@ domain="${container_ip}.$MAGICDNS"
 if ! kubectl get configmap -n kube-system 2>/dev/null | grep -qi cap-values; then
     kubectl create configmap -n kube-system cap-values \
             --from-literal=public-ip="${container_ip}" \
-            --from-literal=services="lb" \
             --from-literal=domain="$domain" \
+            --from-literal=services="hardcoded" \
             --from-literal=platform="minikube"
 fi
