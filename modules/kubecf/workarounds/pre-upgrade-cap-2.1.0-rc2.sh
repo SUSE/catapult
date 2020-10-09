d05771d9fc74b379487f4177fa8c0da1c2df6610
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-10-09 14:50:02 +0200

    Allow pre-upgrade-cap-2.1.0-rc2.sh to be rerun

--- modules/kubecf/workarounds/pre-upgrade-cap-2.1.0-rc2.sh
+++ modules/kubecf/workarounds/pre-upgrade-cap-2.1.0-rc2.sh
@@ -3,6 +3,6 @@
 info "Applying WORKAROUND $(basename ${BASH_SOURCE[0]})"
 set +x # print workaround
 
-kubectl delete qjob --namespace scf dm
+kubectl delete qjob --namespace scf dm --ignore-not-found
 
 debug_mode # reset printing option
