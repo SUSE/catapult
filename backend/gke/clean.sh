f27a0878250381a4f6561a3b161b6a81f1757b98
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-10-09 16:38:26 +0200

    Remove tf locks on gke clean.sh
    We don't care if the previous run has finished successfully

--- backend/gke/clean.sh
+++ backend/gke/clean.sh
@@ -11,6 +11,11 @@ if [ -d "$BUILD_DIR" ]; then
 
     pushd cap-terraform/gke || exit
     terraform init
+
+    # Remove any terraform locks, because we don't care if the previous run
+    # has finished successfully (e.g. on cancel).
+    rm -rf .terraform.*.lock*
+
     terraform destroy -auto-approve
     popd || exit
 
