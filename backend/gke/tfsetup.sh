3295a15ea118de8954f8c5354af2762e81b20005
Mark Yen <mark.yen@suse.com>
2020-10-28 15:10:55 -0700

    fix: GKE: Use jq instead of yq for rendering
    In commit 7127ab4176f026be59507c2d6d2f4d7e5a81568c we changed things to
    use JSON for terraform configs, but using yq because it was listed in
    the deps script.  However, it appears that catapult's dependency
    management is rather confused, and the tests do not actually download
    the dependencies before running things.
    
    Switch to using jq instead, on the basis that the CONFIG mechanism
    already requires jq to be installed to work; that must mean that for the
    tests to pass, the host must already have jq available.  This is really
    more of a workaround than fixing the real issue; refactoring catapult's
    tools dependency management will have to come later (once we figure out
    how to deal with the need jq - need to download jq - need the configs to
    figure out where to put jq - need jq to find configs cycle).

--- backend/gke/tfsetup.sh
+++ backend/gke/tfsetup.sh
@@ -17,31 +17,33 @@ git checkout "${CAP_TERRAFORM_BRANCH}"
 git pull
 
 # Clear out any existing variables file
-echo "" > terraform.tfvars.json
-yq merge --inplace --tojson --prettyPrint terraform.tfvars.json - <<HEREDOC
-project        : "$GKE_PROJECT"
-location       : "$GKE_LOCATION"
-node_pool_name : "$GKE_CLUSTER_NAME"
-instance_count : "$GKE_NODE_COUNT"
-preemptible    : "$GKE_PREEMPTIBLE"
-vm_type        : "UBUNTU"
-gke_sa_key     : "$GKE_CRED_JSON"
-gcp_dns_sa_key : "$GKE_DNSCRED_JSON"
-cluster_labels :
-    catapult-clustername: "$GKE_CLUSTER_NAME"
-    owner : "${OWNER}"
-cluster_name   : "$GKE_CLUSTER_NAME"
-k8s_version    : "latest"
+cat > terraform.tfvars.json <<HEREDOC
+{
+    "project"        : "$GKE_PROJECT",
+    "location"       : "$GKE_LOCATION",
+    "node_pool_name" : "$GKE_CLUSTER_NAME",
+    "instance_count" : "$GKE_NODE_COUNT",
+    "preemptible"    : "$GKE_PREEMPTIBLE",
+    "vm_type"        : "UBUNTU",
+    "gke_sa_key"     : "$GKE_CRED_JSON",
+    "gcp_dns_sa_key" : "$GKE_DNSCRED_JSON",
+    "cluster_labels" : {
+        "catapult-clustername": "$GKE_CLUSTER_NAME",
+        "owner" : "${OWNER}"
+    },
+    "cluster_name"   : "$GKE_CLUSTER_NAME",
+    "k8s_version"    : "latest"
+}
 HEREDOC
 
 if [ -n "${EXTRA_LABELS}" ] ; then
-    yq merge --inplace --tojson --prettyPrint --overwrite terraform.tfvars.json \
-        - <<< "cluster_labels: ${EXTRA_LABELS}"
+    jq --raw-output --argjson labels "${EXTRA_LABELS}" '.cluster_labels *= $labels' terraform.tfvars.json \
+        | sponge terraform.tfvars.json
 fi
 
 if [ -n "${GKE_INSTANCE_TYPE}" ] ; then
-    yq write --inplace --tojson --prettyPrint terraform.tfvars.json \
-        instance_type "${GKE_INSTANCE_TYPE}"
+    jq --argjson type "${GKE_INSTANCE_TYPE}" '.instance_type = $type' terraform.tfvars.json \
+        | sponge terraform.tfvars.json
 fi
 
 if [ -n "${TF_KEY}" ] ; then
